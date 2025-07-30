78671092f3ef875c05fc458a4f4205d4b82f5263
huayuan_computer <17694944948@163.com>
2025-07-30 15:20:48 +0800

    rm -rf share/manuals/{input,output} share/test/ and modify the build.sh and meta.yaml

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ set -e
 set -o pipefail
 
 # cp additional scripts file to ${CONDA_PREFIX}/share
-# bin:			${CONDA_PREFIX}/share/$package_name/bin
-# blacklist:	${CONDA_PREFIX}/share/$package_name/blacklist
-# genomesize:	${CONDA_PREFIX}/share/$package_name/genomesize
-# gsl:			${CONDA_PREFIX}/share/$package_name/gsl
-# manuals:		${CONDA_PREFIX}/share/$package_name/manuals
+# bin:			${CONDA_PREFIX}/share/${package_name}/bin
+# blacklist:	${CONDA_PREFIX}/share/${package_name}/blacklist
+# genomesize:	${CONDA_PREFIX}/share/${package_name}/genomesize
+# gsl:			${CONDA_PREFIX}/share/${package_name}/gsl
+# manuals:		${CONDA_PREFIX}/share/${package_name}/manuals
 mkdir -p ${CONDA_PREFIX}/share/${package_name}
-mkdir -p ${PREFIX}/bin/
+mkdir -p ${CONDA_PREFIX}/bin/
 
 cp -rf ${SRC_DIR}/share/bin ${CONDA_PREFIX}/share/${package_name}
 cp -rf ${SRC_DIR}/share/blacklist ${CONDA_PREFIX}/share/${package_name}
@@ -24,4 +24,4 @@ cp -rf ${SRC_DIR}/share/gsl ${CONDA_PREFIX}/share/${package_name}
 tar xf ${CONDA_PREFIX}/share/${package_name}/gsl/gsl_221.tar.gz -C ${CONDA_PREFIX}/share/${package_name}/gsl/
 rm -rf ${CONDA_PREFIX}/share/${package_name}/gsl/gsl_221.tar.gz
 
-cp -rf ${SRC_DIR}/bins/* ${PREFIX}/bin/
+cp -rf ${SRC_DIR}/bins/* ${CONDA_PREFIX}/bin/
